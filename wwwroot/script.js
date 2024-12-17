1762965a7a03c5f6f6f7c3bf344847eeea8145b9
monikadevi2212 <monikasuman581@gmail.com>
2024-12-17 07:15:10 -0500

    Add files via upload

---
+++ wwwroot/script.js
@@ -0,0 +1,14 @@
+﻿window.onload = function () {
+    // Animation for header
+    let header = document.querySelector('header');
+    let h1 = document.querySelector('h1');
+
+    // Change background color and grow font size
+    header.style.transition = 'all 1s ease-in-out';
+    h1.style.transition = 'all 1s ease-in-out';
+
+    setTimeout(function () {
+        header.style.backgroundColor = '#ff6347';  // Change to a tomato color
+        h1.style.fontSize = '3rem';  // Increase size of text
+    }, 500);
+}
